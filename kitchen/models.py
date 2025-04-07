97fcb8f9bbb6a71e2c05f9a0b6ad9e9efa981f3f
kiran45kumar <kirankiccha2003@gmail.com>
2025-04-07 16:16:07 +0530

    Added Kitchen

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from user.models import User
 # Create your models here.
+class CookingCategory(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=25, null=True, blank=True)
+    sub_category = models.ForeignKey('self',on_delete=models.SET_NULL,null=True, blank=True,related_name='sub_categories')
 class Kitchen(models.Model):
     kitchen_id = models.AutoField(primary_key=True)
-    kitchen_name = models.CharField(max_length=80, null=False, blank=False,unique=True)#required
-    kitchen_desc = models.TextField(max_length=80, null=False, blank=False)#required
+    kitchen_name = models.CharField(max_length=80, null=False, blank=False,unique=True) #required
+    kitchen_desc = models.TextField(max_length=80, null=False, blank=False) #required
     website_url = models.URLField(unique=True, blank=True, null=True) #optional
     business_email = models.EmailField(unique=True, null=True,blank=True) #optional
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True) #required
@@ -12,7 +16,11 @@ class Kitchen(models.Model):
     kitchen_images = models.FileField(upload_to='kitchen_images/',null=True, blank=True) #optional
     kitchen_videos = models.FileField(upload_to='kitchen_videos/',null=True, blank=True) #optional 
     location = models.TextField() #required
+    cooking_category = models.ForeignKey(CookingCategory, on_delete=models.CASCADE, null=True, blank=True)#optional
     rating = models.IntegerField(null=True, blank=True) #will be given by user;
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True) #auto
+    updatedAt = models.DateTimeField(auto_now=True) #auto
+    
+    def __str__(self):
+        return self.kitchen_name
     
